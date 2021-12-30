8a7c4fe93a0cfe5541bdea190fe3004b9d91dc82
codesmack <92647602+codesmack@users.noreply.github.com>
2021-12-30 19:39:39 +0530

    Add files via upload

--- Ng/FirstApp/src/app/app.component.ts
+++ Ng/FirstApp/src/app/app.component.ts
@@ -4,22 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
-})
-export class AppComponent {
   
-  //Inline Data
-  title = 'Welcome to my Second App';
-  userName = 'Guest';
-  showactive = true;
-  isRequired = true;
-  fullName = 'Sam T';
-  txtType = 'time';
-  labelId = "label_0";
+})
 
+export class AppComponent {
+    showAdmission = false;
+    title = "My First App";
+    marginLeft = "30px";
+    userName = "Mike";
 
     constructor(){
-      
+      this.showAdmission = true;
     }
  
-
+    onNameChange(){
+      console.log(this.userName);
+    }
 }
--- Ng/FirstApp/src/app/app.module.ts
+++ Ng/FirstApp/src/app/app.module.ts
@@ -1,15 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UserComponent } from './components/users/user.component';
+import { AdmissionComponent } from './components/admission/admission.component';
+import { HomeComponent } from './components/home/home.component';
+import { BigHeadingDirective } from './directives/highlight.directive';
+import { UserFormComponent } from './components/userform/userform.component';
+import { AddOptionSelectedDirective } from './directives/AddOptionSelectedDirective';
+import { AmazonHomeComponent } from './Amazon/core/components/home/az-home.component';
+import { FirstComponent } from './Class30/first/first.component';
+import { SecondComponent } from './Class30/second/second.component';
+import { LoginComponent } from './class31/login/login.component';
+import { StartComponent } from './class31/start/start.component';
+import { AboutComponent } from './class31/about/about.component';
+import { ContactComponent } from './class31/contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './class31/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdmissionComponent,
+    HomeComponent,
+    BigHeadingDirective,
+    UserFormComponent,
+    AddOptionSelectedDirective,
+    AmazonHomeComponent,
+    FirstComponent,
+    SecondComponent,
+    LoginComponent,
+    StartComponent,
+    AboutComponent,
+    ContactComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        { path: '', redirectTo: 'welcome', pathMatch: "full" },
+        { path: "home", component: StartComponent },
+        { path: "about", component: AboutComponent },
+        { path: "contact", component: ContactComponent },
+        { path: "welcome", component: WelcomeComponent }
+      ]
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [StartComponent]
 })
 export class AppModule { }
